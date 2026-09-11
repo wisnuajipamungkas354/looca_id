cbc455d9b6a53bc5391e2c667fd8ee5f8686a252
Wisnu Aji Pamungkas <wisnuajipamungkas354@gmail.com>
2026-09-11 16:01:10 +0700

    Fix Vercel Analytics integration error in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true, // set to false when using @vercel/analytics@1.4.0
+    },
+  }),
 });
